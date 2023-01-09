5c5de69424a003d3b7651d89db4b2220530eb859
Alex Filip <alexfilippaul@gmail.com>
2023-01-10 00:07:33 +0200

    Add project files.

---
+++ lab4ex5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab4ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrieti o functie recursiva care va calcula suma numerelor de la 1 pana la n, apelati - o si afisati-i rezultatul.
+            Console.WriteLine("Introduceti n ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine( GrandSum(n));
+
+        }
+        static int GrandSum(int n)
+        {
+            if (n == 0)
+            {
+                return n;
+            }
+            return n + GrandSum(n - 1);
+        }
+    }
+}
